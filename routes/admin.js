bd2e23b2ae2f7ba3c1273395be90fc67182725f0
LeGenDaZveR <legendazver@gmail.com>
2026-02-24 18:08:45 +0300

    rebuild deps

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require("multer");
 
 const Admin = require("../models/Admin");
